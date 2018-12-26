f15e5e1359b9b218a1e7459c0b7fdcc3c699fa8c
Martin Iturbide <martin@os2world.com>
2018-12-25 21:04:42 -0500

    1989-09

---
+++ REGBAS.C
@@ -0,0 +1,37 @@
+#define INCL_WIN
+#include <os2.h>
+
+extern MRESULT EXPENTRY ClientWndProc ( long near *, int near *,
+					long near *, long near *);
+
+MRESULT EXPENTRY BasClientWndProc(HWND hwnd, USHORT msg, MPARAM mp1, MPARAM mp2)
+{
+   static long near hwndb;
+   static int near  msgb;
+   static long near mp1b;
+   static long near mp2b;
+   hwndb=(long)hwnd;
+   msgb=(int)msg;
+   mp1b=(long)mp1;
+   mp2b=(long) mp2;
+   return ClientWndProc(&hwndb, &msgb, &mp1b, &mp2b);
+
+} /* BasClientWndProc */
+
+
+long pascal far RegBas(void)
+{
+   return (long) BasClientWndProc;
+
+} /* regbas */
+
+long pascal far MakeLong (long passedlong)
+{
+return passedlong;
+} /* makelong */
+
+void pascal far BreakLong (int hiword, int loword, int *phiword, int *ploword)
+{
+*phiword = hiword;
+*ploword = loword;
+}
